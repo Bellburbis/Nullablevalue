d493032111b8dd3f5fcd4c9b3b8e4293fe8f78f6
Pooja Wadnerkar <pooja.wadnerkar@thinkfirstservices.com>
2021-04-29 19:40:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    { 
+        Console.WriteLine("Please enter a number");
+
+        int UserNumber = int.Parse(Console.ReadLine());
+        
+        switch (UserNumber)
+        {
+            case 10:
+                Console.WriteLine(" Your number is 10");
+                break;
+
+            case 20:
+                Console.WriteLine(" Your number is 20");
+                break;
+
+            case 30:
+                Console.WriteLine(" Your number is 30");
+                break;
+            default:
+                Console.WriteLine("Your number is not 10, 20 and 30");
+                break;
+        }
+    }
+}
